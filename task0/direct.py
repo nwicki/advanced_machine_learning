e8ab0c5ac22ad122616cc6f3bd334e63749808a3
Nicolas <nwicki@ethz.ch>
2021-10-12 10:22:40 +0200

    Task0: Sklearn Linear Regression with Scaled Data

--- task0/direct.py
+++ task0/direct.py
@@ -1,23 +1,16 @@
 import pandas
 import numpy
+import test_func
+import sklearn
+from sklearn import preprocessing
 
 csv_file = 'train.csv'
 data = pandas.read_csv(csv_file)
-x = numpy.asarray(data.iloc[:, 2:]).astype('float32')
-y = numpy.asarray(data['y']).astype('float32')
+x = numpy.asarray(data.iloc[:, 2:]).astype('float64')
+y = numpy.asarray(data['y']).astype('float64')
 # (X^T*X)^-1 * X^T * y = b
 xt = x.transpose()
 b = numpy.matmul(numpy.matmul(numpy.linalg.pinv(numpy.matmul(xt, x)), xt), y)
-# X * b = y
-yhat = numpy.matmul(x, b)
 
-test_data = pandas.read_csv('test.csv')
-test_x = numpy.asarray(test_data.iloc[:, 1:]).astype('float32')
-output = numpy.matmul(test_x, b)
-start = 10000
-end = start + len(output)
-dataframe = pandas.DataFrame(output, index=range(start, end), columns={'y'})
-dataframe.index.name = 'Id'
-dataframe.to_csv('submission_direct.csv')
-
-
+output = numpy.matmul(test_func.get_test_data(), b)
+test_func.test(output, 'direct')
